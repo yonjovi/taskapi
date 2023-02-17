13ff3a265f492564b0bfaf8dd55f9aa2b5f11bd8
Yonatan Rotem <yonatanrotem@Yonatans-Mac-Studio.local>
2023-02-18 01:33:29 +1100

    db_update

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db_username = "task_db_e1hh_user"
-db_password = "723vlQ1dcsrQbVOHsgwiBItUGugXUMVB"
-db_host = "dpg-cfno4hta499f28cskvlg-a.oregon-postgres.render.com"
-db_name = "task_db_e1hh"
+db_username = os.environ['DB_USERNAME']
+db_password = os.environ['DB_PASSWORD']
+db_host = os.environ['DB_HOST']
+db_name = os.environ['DB_NAME']
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{db_username}:{db_password}@{db_host}/{db_name}"
 
