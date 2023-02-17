e0d76826a38a208ade92504c5f6c0bc32077bd6b
Yonatan Rotem <yonatanrotem@Yonatans-Mac-Studio.local>
2023-02-18 00:56:35 +1100

    fix db

--- database.py
+++ database.py
@@ -4,10 +4,8 @@ from sqlalchemy.orm import sessionmaker
 
 db_username = "task_db_e1hh_user"
 db_password = "723vlQ1dcsrQbVOHsgwiBItUGugXUMVB"
-db_host = "dpg-cfno4hta499f28cskvlg-a"
+db_host = "dpg-cfno4hta499f28cskvlg-a.oregon-postgres.render.com"
 db_name = "task_db_e1hh"
-
-
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{db_username}:{db_password}@{db_host}/{db_name}"
 
