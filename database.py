dbaa89e2643191c2b962633f9180fdba0433c8f0
Yonatan Rotem <yonatanrotem@Yonatans-Mac-Studio.local>
2023-03-10 00:53:58 +1100

    update_database

--- database.py
+++ database.py
@@ -3,15 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db_username = os.environ['DB_USERNAME']
-db_password = os.environ['DB_PASSWORD']
-db_host = os.environ['DB_HOST']
-db_name = os.environ['DB_NAME']
 
 db_url = os.environ['DB_URL']
 
-
-# SQLALCHEMY_DATABASE_URL = f"postgresql://{db_username}:{db_password}@{db_host}/{db_name}"
 SQLALCHEMY_DATABASE_URL = db_url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
